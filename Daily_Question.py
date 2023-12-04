21122a62fae0ed2d3bcbe0e9272ebf5d6a8caaef
Max Stoner <83330047+Santymax123@users.noreply.github.com>
2023-12-04 19:38:08 +0800

    Create Daily_Question.py
    File for all my daily python questions

---
+++ Daily_Question.py
@@ -0,0 +1,22 @@
+# Day 2264 Largest 3-Same-Digit Number in String
+
+def largestGoodInteger(self, num):
+	"""
+	:type num: str
+	:rtype: str
+	"""
+	last_num = num[0]
+	count = 0
+	current_best = ""
+	
+	for number in num[1:]:
+			if number == last_num and number > current_best:
+					count += 1
+			else:
+					count = 0
+					last_num = number
+			
+			if count == 2:
+					current_best = number
+	
+	return "" + current_best + current_best + current_best
